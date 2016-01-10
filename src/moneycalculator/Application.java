ab6282013437234ea266151612ce850fb30dd07c
irequ_000 <irequ_000@192.168.1.40>
2016-01-10 18:36:13 +0000

    segunda versión

---
+++ src/moneycalculator/Application.java
@@ -0,0 +1,22 @@
+package moneycalculator;
+
+import moneycalculator.control.ExchangeCommand;
+import moneycalculator.persistence.ExchangeRateGetter;
+import moneycalculator.ui.ShowNewCurrencyDialog;
+import moneycalculator.ui.ShowMoneyToChangeDialog;
+import moneycalculator.ui.ShowMoneyDisplay;
+
+public class Application {
+
+    public static void main(String[] args) {
+        ExchangeCommand exchangeCommand = new ExchangeCommand(
+                new ShowMoneyToChangeDialog(),
+                new ShowNewCurrencyDialog(),
+                new ExchangeRateGetter(), 
+                new ShowMoneyDisplay()
+        );
+        exchangeCommand.execute();
+                
+    }
+    
+}
